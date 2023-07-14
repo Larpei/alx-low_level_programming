776fb7494e067ab75ab2a8fcffccc8d5a047893a
Andrew Larpei <andrewlarpei@gmail.com>
2023-07-14 14:58:08 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,8 @@ int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for (i = 0; i < 26; i++)
-	{
-	putchar(alp[i]);
-	}
+	for (i = 0; i < 26; i++){
+	putchar(alp[i]);}
 	putchar('\n');
 	return (0);
 }
